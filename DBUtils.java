8159a839d50a42d8fc1426841a7cf29c799758f1
devopstrainingblr <devopstrainingblrr@gmail.com>
2024-10-15 13:41:25 +0000

    Updated

--- DBUtils.java
+++ DBUtils.java
@@ -3,4 +3,5 @@ This file contains DB release code
 Hello added this line
 Hello added this line
 Hello adding in development branch
-Hello added this line in git cherry -pickup  
+Hello added this line in git cherry -pickup 
+Hello added by Divyateja 
